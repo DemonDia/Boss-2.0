914ba273624f83ee36452d98376c6aa9275e9863
Setsuna Yukiii <siang-meng@live.com>
2023-06-03 14:27:13 +0800

    feat: get medians & prof names per term

--- Controllers/ModController.js
+++ Controllers/ModController.js
@@ -3,6 +3,22 @@ const Mod = require("../Models/ModModel");
 // =========================Helper functions=========================
 
 // =========================Read=========================
+const getProfNamesTerm = async (req, res) => {
+    const { year, term } = req.params;
+    try {
+        const selectedProfs = await Mod.find(
+            { Term: year + " Term " + term },
+            { Instructor: 1 }
+        ).distinct("Instructor");
+        return res.json({
+            data: selectedProfs,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
 const getModNamesTerm = async (req, res) => {
     // const start = Date.now();
     const { year, term } = req.params;
@@ -27,6 +43,11 @@ const getModsCodesTerm = async (req, res) => {
             { Term: year + " Term " + term },
             { Description: 1 }
         ).distinct("Course_Code");
+        if (selectedMods.length === 0) {
+            return res.status(404).json({
+                message: "No records found",
+            });
+        }
         return res.json({
             data: selectedMods,
         });
@@ -36,8 +57,40 @@ const getModsCodesTerm = async (req, res) => {
     }
 };
 
-const getAveragePriceMod = async (req, res) => {
-    const [round, window, code] = req.params;
+const getAverageMedianMod = async (req, res) => {
+    const { term, year, round, window, code, prof } = req.params;
+    console.log({ term, year, round, window, code, prof } );
+    try {
+        console.log(`Round ${round} Window ${window}`);
+        const selectedMods = await Mod.find(
+            {
+                Course_Code: code,
+                Bidding_Window: `Round ${round} Window ${window}`,
+                Instructor: prof,
+            },
+            { Median_Bid: 1 }
+        );
+        if (selectedMods.length === 0) {
+            return res.status(404).json({
+                message: "No records found",
+            });
+        }
+        let medians = [];
+        selectedMods.forEach((mod) => {
+            medians.push(mod.Median_Bid);
+        });
+        return res.json({
+            data: medians,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
 };
 
-module.exports = { getModNamesTerm, getModsCodesTerm };
+module.exports = {
+    getProfNamesTerm,
+    getModNamesTerm,
+    getModsCodesTerm,
+    getAverageMedianMod,
+};
--- Routes/ModRoutes.js
+++ Routes/ModRoutes.js
@@ -8,6 +8,8 @@ const modController = require("../Controllers/ModController");
 // ===========================all routes===========================
 router.get("/name/t/:term/y/:year", modController.getModNamesTerm);
 router.get("/code/t/:term/y/:year", modController.getModsCodesTerm);
+router.get("/prof/t/:term/y/:year", modController.getProfNamesTerm);
+router.get("/median/:term/:year/:round/:window/:code/:prof", modController.getAverageMedianMod);
 
 // ===========================export routes===========================
 module.exports = router;
